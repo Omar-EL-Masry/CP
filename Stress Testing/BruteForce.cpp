501d636743f111300f60003bead5f23852f9682a
O_Elmasry <143853170+Omar-EL-Masry@users.noreply.github.com>
2024-08-16 07:12:35 +0300

    BruteForce.cpp

---
+++ Stress Testing/BruteForce.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+using namespace std;
+#include "debug.h"
+#define endl '\n'
+#define int long long        
+const long long mod = 998244353, Mxn = 1e5 + 5, INF = 1e18;
+void solve()
+{
+        
+}
+signed main()
+{
+        // Timer timer;
+        ios::sync_with_stdio(0);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        freopen("test.in", "r", stdin);
+        freopen("test2.out", "w", stdout);
+        // freopen("file.in","r",stdin);
+        // freopen("file.out","w",stdout);
+        int tc = 1;
+        // cin >> tc;
+        while (tc--)
+        {
+                solve();
+        }
+}
+
