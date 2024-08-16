8ae11c6ffcd3f3e24dfab7169c4345f1ab5e8e52
O_Elmasry <143853170+Omar-EL-Masry@users.noreply.github.com>
2024-08-16 07:14:38 +0300

    Cordinate_Compression.cpp

---
+++ Data Structures & Algorithms/Cordinate_Compression.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Cordinate_Compression{
+        set<int>vals;
+        map<int, int>val_to_cord, cord_to_val;
+        void init(set<int>s){
+                vals = s;
+                int idx = 1;
+                for(auto i : vals){
+                        cord_to_val[idx] = i;
+                        val_to_cord[i] = idx++;
+                }
+        }
+        int incr(int x){
+                return val_to_cord[x];
+        }
+        int decr(int idx){
+                return cord_to_val[idx];
+        }
+};
+int main()
+{
+        return 0;
+}
