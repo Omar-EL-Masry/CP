7a0679927d7b5d42fee2cfeee73e3a3950a64007
O_Elmasry <143853170+Omar-EL-Masry@users.noreply.github.com>
2024-08-16 07:10:17 +0300

    Solution.cpp

---
+++ Template/Solution.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+using namespace std;
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define dbg(...)
+#define Timer int
+#endif
+#define endl '\n'
+#define int long long        
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+const long long mod = 998244353, Mxn = 1e5 + 5, INF = 1e18;
+void solve()
+{
+        
+}
+signed main()
+{
+        Timer timer;
+        ios::sync_with_stdio(0);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+#ifdef LOCAL
+        FileRedirect("test");
+#endif
+        // freopen("file.in","r",stdin);
+        // freopen("file.out","w",stdout);
+        int tc = 1;
+        // cin >> tc;
+        while (tc--)
+        {
+                solve();
+        }
+}
+
