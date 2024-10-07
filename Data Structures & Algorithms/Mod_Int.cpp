3d84fa9d4342f44b9082d34c7cf8d3ea45e0eb7a
O_Elmasry <143853170+Omar-EL-Masry@users.noreply.github.com>
2024-10-07 18:31:19 +0300

    Mod_Int.cpp

--- Data Structures & Algorithms/Mod_Int.cpp
+++ Data Structures & Algorithms/Mod_Int.cpp
@@ -2,106 +2,107 @@
 using namespace std;
 #define int long long
 const int Mxn = 200005, mod = 1000000007;
-template<int Mod = mod> struct Mint{
+template<int Mod = mod> struct Mod_int{
   int val = 0;
-  friend istream &operator>>(istream &is, Mint &x){
+  friend istream &operator>>(istream &is, Mod_int &x){
     is >> x.val;
     x.val = (x.val + Mod) % Mod;
     return is;
   }
-  friend ostream &operator<<(ostream &os, Mint x){
+  friend ostream &operator<<(ostream &os, Mod_int x){
     return os << x.val;
   }
-  Mint(){};
-  Mint(int _val){
+  Mod_int(){};
+  Mod_int(int _val){
     val = (_val + Mod) % Mod;
   }
-  Mint operator+(Mint rhs){
-    return Mint((this -> val + rhs.val) % Mod);
+  Mod_int operator+(Mod_int rhs){
+    return Mod_int((this -> val + rhs.val) % Mod);
   }
-  Mint operator%(int M){
-    Mint ret;
+  Mod_int operator%(int M){
+    Mod_int ret;
     ret.val = (val + M) % M;
     return ret;
   }
-  Mint& operator=(int x){
+  Mod_int& operator=(int x){
     val = (x + Mod) % Mod;
     return *this;
   }
-  Mint& operator=(Mint x){
+  Mod_int& operator=(Mod_int x){
     val = (x.val + Mod) % Mod;
     return *this;
   }
-  Mint operator-(Mint rhs){
+  Mod_int operator-(Mod_int rhs){
     int ret = val - rhs.val + Mod;
-    return Mint(ret % Mod);
+    return Mod_int(ret % Mod);
   }
-  Mint operator*(Mint rhs){
-    return Mint((val * rhs.val) % Mod);
+  Mod_int operator*(Mod_int rhs){
+    return Mod_int((val * rhs.val) % Mod);
   }
-  friend Mint power(Mint a, int b){
-    if(b == 0)return Mint(1);
-    Mint res = power(a, b / 2);
+  friend Mod_int power(Mod_int a, int b){
+    if(b == 0)return Mod_int(1);
+    Mod_int res = power(a, b / 2);
     res = res * res;
     if(b % 2){
       res = res * a;
     }
     return res;
   }
-  friend Mint power(Mint a, Mint b){
+  friend Mod_int power(Mod_int a, Mod_int b){
     return power(a, b.val);
   }
-  Mint inv(){
-    return power(Mint(val), Mod - 2);
+  Mod_int inv(){
+    return power(Mod_int(val), Mod - 2);
   }
-  Mint operator/(Mint rhs){
+  Mod_int operator/(Mod_int rhs){
     return *this * rhs.inv();
   }
-  Mint &operator+=(Mint rhs){
+  Mod_int &operator+=(Mod_int rhs){
     return *this = *this + rhs;
   }
-  Mint &operator-=(Mint rhs){
+  Mod_int &operator-=(Mod_int rhs){
     return *this = *this - rhs;
   }
-  Mint &operator*=(Mint rhs){
+  Mod_int &operator*=(Mod_int rhs){
     return *this = *this * rhs;
   }
-  Mint &operator/=(Mint rhs){
+  Mod_int &operator/=(Mod_int rhs){
     return *this = *this / rhs;
   }
-  Mint &operator%=(int M){
+  Mod_int &operator%=(int M){
     val = (val + M) % M;
     return *this;
   } 
-  bool operator==(Mint rhs){
+  bool operator==(Mod_int rhs){
     return (val == rhs.val);
   }
-  bool operator<(Mint rhs){
+  bool operator<(Mod_int rhs){
     return (val < rhs.val);
   }
-  bool operator<=(Mint rhs){
+  bool operator<=(Mod_int rhs){
     return (val <= rhs.val);
   }
-  bool operator>(Mint rhs){
+  bool operator>(Mod_int rhs){
     return (val > rhs.val);
   }
-  bool operator>=(Mint rhs){
+  bool operator>=(Mod_int rhs){
     return (val >= rhs.val);
   }
 };
 int Mxm = 1000005;
-vector<Mint<mod>>fact(Mxm), inv(Mxm);
+using Mint = Mod_int<mod>;
+vector<Mint>fact(Mxm), inv(Mxm);
 void preproccess(){
   fact[0] = fact[1] = 1;
   for(int i = 2;i < Mxm;i++){
-    fact[i] = fact[i - 1] * Mint(i);
+    fact[i] = fact[i - 1] * Mod_int(i);
   }
   inv[Mxm - 1] = fact[Mxm - 1].inv();
   for(int i = Mxm - 2;i >= 0;i--){
-    inv[i] = inv[i + 1] * Mint(i + 1);
+    inv[i] = inv[i + 1] * Mod_int(i + 1);
   }
 }
-Mint<mod> nCr(int a, int b){
+Mod_int<mod> nCr(int a, int b){
   if(b > a)return 0;
   return fact[a] * inv[a - b] * inv[b]; 
 }
