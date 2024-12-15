e684540cc562c5aa0dbe12082ed6fd9caec31986
O_Elmasry <143853170+Omar-EL-Masry@users.noreply.github.com>
2024-12-15 19:44:26 +0200

    BIT.cpp

---
+++ Data Structures & Algorithms/BIT.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+template<typename T> struct BIT{
+  int sz;
+  vector<T>tree;
+  BIT(){};
+  BIT(int N){
+    sz = N + 1;
+    tree.resize(sz);
+  }
+  int lsb(int x){
+    return (x & -x);
+  }
+  void update(int idx, T x){
+    for(;idx < sz;idx += lsb(idx)){
+      tree[idx] += x;
+    }
+  }
+  T get(int idx){
+    T sum = 0;
+    for(;idx;idx -= lsb(idx)){
+      sum += tree[idx];
+    }
+    return sum;
+  }
+  T get(int l, int r){
+    return get(r) - get(l - 1);
+  }
+};
+int main(){
+  return 0;
+}
