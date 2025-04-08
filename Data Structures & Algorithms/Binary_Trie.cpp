a90638a723ad9109a1cf24806c5ddcf357dd26dd
O_Elmasry <143853170+Omar-EL-Masry@users.noreply.github.com>
2025-04-08 17:07:14 +0200

    Update Binary_Trie.cpp

--- Data Structures & Algorithms/Binary_Trie.cpp
+++ Data Structures & Algorithms/Binary_Trie.cpp
@@ -1,45 +1,51 @@
-  #include<bits/stdc++.h>
-  using namespace std;
-  struct Trie{
-    struct Node{
-      Node *child[2];
-      Node(){
-        memset(child, 0, sizeof child);
-      }
+struct Trie {
+    struct Node {
+        Node *child[2];
+        int cnt;
+        Node() : cnt(0) {
+            memset(child, 0, sizeof(child));
+        }
     };
     Node *root;
-    Trie(){
-      root = new Node();
+    Trie() {
+        root = new Node();
     }
-    void add(int x){
-      Node *node = root;
-      for(int i = 31;i >= 0;i--){
-        int bit = (x >> i) & 1;
-        if(node -> child[bit] == NULL){
-          node -> child[bit] = new Node();
+    void add(int x) {
+        Node *node = root;
+        node->cnt++;
+        for (int i = 31; i >= 0; i--) {
+            int bit = (x >> i) & 1;
+            if (node->child[bit] == NULL) {
+                node->child[bit] = new Node();
+            }
+            node = node->child[bit];
+            node->cnt++;
         }
-        node = node -> child[bit];
-      }
     }
-    int get(int x){
-      Node *node = root;
-      int res = 0;
-      for(int i = 31;i >= 0;i--){
-        int bit = (x >> i) & 1;
-        if(node -> child[!bit] != NULL){
-          res += (1ll << i);
-          node = node -> child[!bit];
+
+    int get(int x) {
+        Node *node = root;
+        int res = 0;
+        for (int i = 31; i >= 0; i--) {
+            int bit = (x >> i) & 1;
+            if (node->child[!bit] != NULL && node->child[!bit]->cnt > 0) {
+                res |= (1 << i);
+                node = node->child[!bit];
+            } else if (node->child[bit] != NULL && node->child[bit]->cnt > 0) {
+                node = node->child[bit];
+            } else {
+                assert(0);
+            }
         }
-        else if(node -> child[bit] != NULL){
-          node = node -> child[bit];
-        }
-        else{
-          assert(0);
-        }
-      }
-      return res;
+        return res;
     }
-  };
-  int main(){
-    return 0;
-  }
+    void remove(int x) {
+        Node *node = root;
+        node->cnt--;
+        for (int i = 31; i >= 0; i--) {
+            int bit = (x >> i) & 1;
+            node = node->child[bit];
+            node->cnt--;
+        }
+    }
+};
